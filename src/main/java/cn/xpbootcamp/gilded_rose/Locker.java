d3e1d2f894769ef96dc55d6123ce8dc3a3a93ab5
Song Xu <song.xu@thoughtworks.com>
2020-03-25 20:32:21 +0800

    refactor Locker

--- src/main/java/cn/xpbootcamp/gilded_rose/Locker.java
+++ src/main/java/cn/xpbootcamp/gilded_rose/Locker.java
@@ -4,38 +4,36 @@ import com.google.common.annotations.VisibleForTesting;
 
 public class Locker {
 
-    private boolean[] blocks = new boolean[19];
-    private Ticket[] tickets = new Ticket[19];
+    private String[] ticketIds = new String[19];
 
     public Ticket storeIn() {
-        int index = findFirstAvailableIndex(blocks);
+        int index = findFirstAvailableIndex(ticketIds);
         if (index == -1) {
             throw new IllegalStateException("locker is full");
         }
-        blocks[index] = true;
+        ticketIds[index] = "ticketId";
         final Ticket ticket = new Ticket();
         ticket.id = "ticketId";
         ticket.blockNumber = index;
-        tickets[index] = ticket;
         return ticket;
     }
 
     @VisibleForTesting
     void storeInIndex(int index) {
-        blocks[index] = true;
+        ticketIds[index] = "ticketId";
     }
 
-    private int findFirstAvailableIndex(boolean[] blocks) {
-        for (int i = 0; i < blocks.length; i++) {
-            if (!blocks[i]) return i;
+    private int findFirstAvailableIndex(String[] ticketIds) {
+        for (int i = 0; i < ticketIds.length; i++) {
+            if (ticketIds[i] == null) return i;
         }
         return -1;
     }
 
     public int takeOut(String ticketId) {
         for(int i = 0; i < 19; i++){
-            if(tickets[i].id == ticketId){
-                return tickets[i].blockNumber;
+            if(ticketId.equals(ticketIds[i])){
+                return i;
             }
         }
         return -1;
