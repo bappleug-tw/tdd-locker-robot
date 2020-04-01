46e0de863148cacba8d5472a1feac8eaeb1a2143
Song Xu <song.xu@thoughtworks.com>
2020-04-01 21:12:04 +0800

    store bag fail when all the lockers are full

--- src/test/java/cn/xpbootcamp/gilded_rose/PrimaryLockerRobot.java
+++ src/test/java/cn/xpbootcamp/gilded_rose/PrimaryLockerRobot.java
@@ -6,10 +6,9 @@ public class PrimaryLockerRobot {
 
     public Ticket storeBag(List<Locker> lockers, Bag bag) {
         for (Locker locker : lockers) {
-            try{
+            try {
                 return locker.store(bag);
-            }catch (LockerFullException e){
-                continue;
+            } catch (LockerFullException ignored) {
             }
         }
         throw new LockerFullException();
--- src/test/java/cn/xpbootcamp/gilded_rose/PrimaryLockerRobotTest.java
+++ src/test/java/cn/xpbootcamp/gilded_rose/PrimaryLockerRobotTest.java
@@ -6,40 +6,53 @@ import org.junit.jupiter.api.Test;
 import java.util.ArrayList;
 import java.util.List;
 
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
 
 public class PrimaryLockerRobotTest {
     @Nested
     class when_store_bags {
         @Test
-        void should_store_bag_in_the_first_locker_success_and_return_ticket_given_first_locker_has_room(){
-
+        void should_store_bag_in_the_first_locker_success_and_return_ticket_given_first_locker_has_room() {
             PrimaryLockerRobot primaryLockerRobot = new PrimaryLockerRobot();
             Bag bag = new Bag();
             List<Locker> lockers = new ArrayList<>();
             lockers.add(new Locker(2));
             lockers.add(new Locker(2));
             Ticket ticket = primaryLockerRobot.storeBag(lockers, bag);
+
             assertThat(ticket).isNotNull();
             assertThat(lockers.get(0).takeOut(ticket)).isEqualTo(bag);
-
         }
 
         @Test
-        void should_store_bag_in_the_second_locker_success_and_return_ticket_given_only_the_first_locker_is_full(){
-
+        void should_store_bag_in_the_second_locker_success_and_return_ticket_given_only_the_first_locker_is_full() {
             PrimaryLockerRobot primaryLockerRobot = new PrimaryLockerRobot();
-
             List<Locker> lockers = new ArrayList<>();
-            Locker locker = new Locker(1);
-            locker.store(new Bag());
-            lockers.add(locker);
+            lockers.add(getFullLocker());
             lockers.add(new Locker(2));
             Bag bag = new Bag();
             Ticket ticket = primaryLockerRobot.storeBag(lockers, bag);
+
             assertThat(ticket).isNotNull();
             assertThat(lockers.get(1).takeOut(ticket)).isEqualTo(bag);
-
         }
+
+        @Test
+        void should_store_bag_fail_given_all_the_lockers_are_full() {
+            PrimaryLockerRobot primaryLockerRobot = new PrimaryLockerRobot();
+            List<Locker> lockers = new ArrayList<>();
+            lockers.add(getFullLocker());
+            lockers.add(getFullLocker());
+
+            assertThatThrownBy(() -> primaryLockerRobot.storeBag(lockers, new Bag()))
+                    .isInstanceOf(LockerFullException.class);
+        }
+    }
+
+    private Locker getFullLocker() {
+        Locker locker = new Locker(1);
+        locker.store(new Bag());
+        return locker;
     }
 }
