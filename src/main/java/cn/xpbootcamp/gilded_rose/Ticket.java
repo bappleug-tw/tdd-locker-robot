a9d4b7cb05f4678b7036081d497aa7575ccf1b17
RayMin <rmin@thoughtworks.com>
2020-04-01 19:48:07 +0800

    refactor test and impl to match the requirement

--- src/main/java/cn/xpbootcamp/gilded_rose/Ticket.java
+++ src/main/java/cn/xpbootcamp/gilded_rose/Ticket.java
@@ -2,18 +2,12 @@ package cn.xpbootcamp.gilded_rose;
 
 public class Ticket {
     private String id;
-    private int blockNumber;
 
-    public Ticket(int blockNumber) {
+    public Ticket() {
         this.id = HashUtils.generate();
-        this.blockNumber = blockNumber;
     }
 
     public String getId() {
         return id;
-    }
-
-    public int getBlockNumber() {
-        return blockNumber;
     }
 }
